ce44631b9f619873f96f4ec1c79b750016d65477
dongwoo <qorehddn123@naver.com>
2020-07-22 00:03:01 +0900

    50% clear

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css"
 //import PropTypes from "prop-types"
-
 // function renderFood(dish) {
 // console.log(dish)
 //     return (
@@ -9,23 +11,56 @@ import React from 'react';
 // }
 class App extends React.Component {
     state = {
-        isLoading: true
+        isLoading: true,
+        movies: []
+    }
+
+    getMovies = async () => {
+        const {
+            data: {             //data안에 있는 data data안에있는 movies
+                data: {movies}
+            }
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json" +
+            "?sort_by=rating");
+        // this.setState({movies}) 14 20 줄 movies
+        this.setState({movies, isLoading: false})        //위와같다
+
     }
 
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({isLoading: false});
-        }, 3000)
+        this.getMovies();
     }
 
+//    "https://yts-proxy.now.sh/list_movies.json"
     render() {
-        const {isLoading} = this.state
+        const {isLoading, movies} = this.state//this.state.movies
         return (
-            <div>
+
+            <section className="container">
                 {
-                    isLoading ? "Londing" : "we are ready"
-                }
-            </div>
+                    isLoading ? (
+
+                        <div className="loader">
+                            <span className="Loader__text">"Londing"</span>
+                        </div>
+
+                    ) : (
+                        <div className="movies">
+                            {movies.map(movie =>(
+
+                            <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                            />
+                            ))}
+                        </div>
+                    )}
+            </section>
         )
     }
 }
