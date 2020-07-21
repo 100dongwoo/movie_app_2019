3a087906e11490c5706fec79af6d4e1ec7dc0842
dongwoo <qorehddn123@naver.com>
2020-07-21 21:20:44 +0900

    setTimeout_example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import PropTypes from "prop-types"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function renderFood(dish) {
+// console.log(dish)
+//     return (
+//         <Food name={dish.name} picture={dish.image}/>
+//     )
+// }
+class App extends React.Component {
+    state = {
+        isLoading: true
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({isLoading: false});
+        }, 3000)
+    }
+
+    render() {
+        const {isLoading} = this.state
+        return (
+            <div>
+                {
+                    isLoading ? "Londing" : "we are ready"
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
